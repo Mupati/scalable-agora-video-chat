d876c604b581a535bd3f40c4fade765ba81bcaca
Mupati <scientificgh@gmail.com>
2020-12-07 14:27:53 +0000

    Add video chat functionality

--- index.js
+++ index.js
@@ -1,64 +1,102 @@
+let app = new Vue({
+  el: "#app",
+  data: {
+    message: "Hello Vue!",
+    client: null,
+  },
 
-var client = AgoraRTC.createClient({mode: 'live', codec: "h264"});
+  mounted() {
+    this.initializeAgora();
+  },
 
-client.init(<initializing>, function () {
-console.log("AgoraRTC client initialized");
-}, function (err) {
-console.log("AgoraRTC client init failed", err);
+  methods: {
+    initializeAgora() {
+      this.client = AgoraRTC.createClient({ mode: "rtc", codec: "h264" });
+      this.client.init(
+        "396e04646ef344e5a6c69304f56f59c0",
+        function () {
+          console.log("AgoraRTC client initialized");
+        },
+        function (err) {
+          console.log("AgoraRTC client init failed", err);
+        }
+      );
+
+      this.client.join(
+        "006396e04646ef344e5a6c69304f56f59c0IAB1UxKZ6t9v8exa0PtrA0FO6CuwkH+moFN67mMJOEpe5HavCmIAAAAAEAAWal0mdajOXwEAAQB0qM5f",
+        "video_chat",
+        null,
+        (uid) => {
+          console.log("User " + uid + " join channel successfully");
+          this.createLocalStream();
+        },
+        function (err) {
+          console.log("Join channel failed", err);
+        }
+      );
+
+      //   Register event listeners
+
+      this.client.on("stream-published", function (evt) {
+        console.log("Publish local stream successfully");
+        console.log(evt);
+      });
+
+      //subscribe remote stream
+      this.client.on("stream-added", ({ stream }) => {
+        console.log("New stream added: " + stream.getId());
+        this.client.subscribe(stream, function (err) {
+          console.log("Subscribe stream failed", err);
+        });
+      });
+
+      this.client.on("stream-subscribed", ({ stream: remoteStream }) => {
+        // Attach remote stream to the remote-video div
+        remoteStream.play("remote-video");
+        this.client.publish(remoteStream);
+      });
+
+      this.client.on("stream-removed", ({ stream }) => {
+        console.log(String(stream.getId()));
+        stream.close();
+      });
+
+      this.client.on("peer", ({ stream }) => {
+        stream.close();
+      });
+    },
+
+    createLocalStream() {
+      let localStream = AgoraRTC.createStream({
+        audio: true,
+        video: true,
+      });
+
+      // Initialize the local stream
+      localStream.init(
+        () => {
+          // Play the local stream
+          localStream.play("local-video");
+          // Publish the local stream
+          this.client.publish(localStream, (err) => {
+            console.log("publish local stream", err);
+          });
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+    },
+
+    leaveChannel() {
+      this.client.leave(
+        function () {
+          console.log("Leave channel successfully");
+        },
+        function (err) {
+          console.log("Leave channel failed");
+        }
+      );
+    },
+  },
 });
-
-{/* initializing */}
-
-
-localStream.init(function() {
-console.log("getUserMedia successfully");
-localStream.play('agora_local');
-}, function (err) {
-console.log("getUserMedia failed", err);
-});
-
-
-{/* enable video */}
-
-
-client.join(<TOKEN_OR_KEY>, <CHANNEL_NAME>, <UID>, function(uid) {
-    console.log("User " + uid + " join channel successfully");
-}, function(err) {
-    console.log("Join channel failed", err);
-});
-
-//publish local stream
-client.publish(localStream, function (err) {
-    console.log("Publish local stream error: " + err);
-});
-
-client.on('stream-published', function (evt) {
-    console.log("Publish local stream successfully");
-});
-
-//subscribe remote stream
-client.on('stream-added', function (evt) {
-    var stream = evt.stream;
-    console.log("New stream added: " + stream.getId());
-    client.subscribe(stream, function (err) {
-    console.log("Subscribe stream failed", err);
-    });
-});
-
-client.on('stream-subscribed', function (evt) {
-    var remoteStream = evt.stream;
-    console.log("Subscribe remote stream successfully: " + remoteStream.getId());
-    remoteStream.play('agora_remote' + remoteStream.getId());
-})
-
-
-{/* Leaving Channel */}
-
-client.leave(function () {
-    console.log("Leave channel successfully");
-}, function (err) {
-    console.log("Leave channel failed");
-});
-
-{/* Invite friends to join the demo */}
-https://console.agora.io/invite?sign=4ae163a75a8d082e158c2ef64d840267%3A664021a9bf6c92b36ce409210188ffabbb89d92c613204ef814468a81988358f
